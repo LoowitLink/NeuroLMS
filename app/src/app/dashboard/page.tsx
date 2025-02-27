01dc8ac23d27e0e28cec0ec3cfafdc28354766e2
Mason G <63264652+The-MG@users.noreply.github.com>
2025-02-27 15:47:42 -0800

    legal disclaimers

--- app/src/app/dashboard/page.tsx
+++ app/src/app/dashboard/page.tsx
@@ -1,4 +1,5 @@
 import { AppSidebar } from '@/components/app-sidebar';
+import { CourseCard } from '@/components/course-list/CourseCard';
 import {
     Breadcrumb,
     BreadcrumbItem,
@@ -35,6 +36,7 @@ export default function Page() {
                     </div>
                 </header>
                 <div className='flex flex-1 flex-col gap-4 p-4'>
+                    <CourseCard />
                     <div className='grid auto-rows-min gap-4 md:grid-cols-3'>
                         <div className='aspect-video rounded-xl bg-muted/50' />
                         <div className='aspect-video rounded-xl bg-muted/50' />
